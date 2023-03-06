bb35323e9dc75667973b7d328ce92bc35ca4c9de
Guilherme Rocha <97315490+guilhermescr@users.noreply.github.com>
2023-03-06 17:03:32 -0300

    changed some images and updated README.md

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
-# GreenFood Fictional Restaurant
+<span id="beginning"></span>
 
-Landing Page for a fictional restaurant.
+# Green Food Fictional Restaurant
 
-<img src="./assets/images/greenfood-homepage.png" alt="GreenFood Homepage" />
+<p>Landing Page for a fictional restaurant.</p>
 
-This project was inspired by this template on Figma: [click here](https://www.figma.com/file/m5YxS94r1o1HTmB1bETkSn/Restaurant-UI-Web-Kit-(Community)?node-id=0%3A1&t=Hy4hyfpuvlDjhb12-1).
+<img src="./greenfood-homepage.png" alt="GreenFood Homepage" />
+
+## Techs & Tools:
+
+<ul>
+  <li>HTML</li>
+  <li>TailwindCSS (and a bit of CSS)</li>
+  <li>JavaScript</li>
+  <li>Git</li>
+  <li>TinyPNG (for image compression)</li>
+</ul>
+
+*This project was inspired by this template on Figma: **[click here](<https://www.figma.com/file/m5YxS94r1o1HTmB1bETkSn/Restaurant-UI-Web-Kit-(Community)?node-id=0%3A1&t=Hy4hyfpuvlDjhb12-1>).***
+
+## Não pare por aqui!
+
+<p>Gostou do meu projeto? Então me acompanhe nas redes sociais, principalmente no LinkedIn, para ficar por dentro das minhas últimas aplicações!</p>
+
+- [Instagram](https://www.instagram.com/devguiga/)
+- [Linkedin](https://www.linkedin.com/in/guilhermescr/)
+- [Youtube](https://www.youtube.com/@devguiga)
+
+_Developed by Guilherme Rocha - 2023._
+
+[⬆ Voltar ao topo](#beginning)<br>
---
+++ src/en/assets/js/index.js
@@ -0,0 +1,145 @@
+function reduceURL() {
+  let pathname = location.pathname;
+
+  if (pathname.includes('index')) {
+    pathname = pathname
+      .replace(pathname.slice(pathname.search('index')), '')
+      .trim();
+  }
+
+  history.replaceState(
+    {
+      id: 'Green Food Restaurant',
+      source: 'web'
+    },
+    'Green and Tasty Meals',
+    `${location.origin}${pathname}`
+  );
+}
+
+document.querySelectorAll('.clearUrlItemHandler').forEach(headerNavItem => {
+  headerNavItem.addEventListener('click', () => {
+    setTimeout(reduceURL, 10);
+  });
+});
+
+document.body.onload = () => {
+  reduceURL();
+  switchHeaderPosition();
+};
+
+// Sticky Header
+const header = document.getElementById('header');
+const sticky = header.offsetTop;
+
+function switchHeaderPosition() {
+  if (window.scrollY > sticky) {
+    header.classList.add('sticky');
+    header.classList.add('bg-gradient-to-b');
+    header.classList.add('from-lime-600');
+    header.classList.add('to-green-600');
+  } else {
+    header.classList.remove('sticky');
+    header.classList.remove('bg-gradient-to-b');
+    header.classList.remove('from-lime-600');
+    header.classList.remove('to-green-600');
+  }
+}
+
+window.onscroll = switchHeaderPosition;
+
+// Hamburger Icon Animation
+const HAMBURGER_ICON = document.querySelector('.open-responsive-navbar-button');
+const RESPONSIVE_MENU = document.querySelector('.responsive-menu');
+const RESPONSIVE_MENU_ASIDE_BAR = document.querySelector(
+  '.responsive-menu__aside-bar'
+);
+const RESPONSIVE_MENU_OPTIONS = document.querySelectorAll(
+  '.responsive-menu__option'
+);
+
+function changeResponsiveMenuAsideBarGradient(windowScrollY) {
+  if (!windowScrollY) {
+    RESPONSIVE_MENU_ASIDE_BAR.classList = `responsive-menu__aside-bar absolute body-img-url bg-center bg-no-repeat bg-cover bg-fixed flex flex-col gap-6 h-full left-[20%] w-[80%] p-4`;
+  } else {
+    RESPONSIVE_MENU_ASIDE_BAR.classList = `responsive-menu__aside-bar absolute bg-gradient-to-b from-green-600 to-lime-500 flex flex-col gap-6 h-full left-[20%] w-[80%] p-4`;
+  }
+}
+
+function toggleHamburgerIcon() {
+  HAMBURGER_ICON.classList.toggle('open');
+
+  changeResponsiveMenuAsideBarGradient(window.scrollY);
+  if (!window.scrollY) {
+    // no gradient
+  } else {
+    // has gradient
+  }
+
+  if (HAMBURGER_ICON.classList.contains('open')) {
+    // open menu
+    document.body.style.overflowY = 'hidden';
+    RESPONSIVE_MENU.classList.remove('hidden');
+  } else {
+    // close menu
+    document.body.style.overflowY = 'auto';
+    RESPONSIVE_MENU.classList.add('hidden');
+  }
+}
+
+HAMBURGER_ICON.addEventListener('click', toggleHamburgerIcon);
+RESPONSIVE_MENU_OPTIONS.forEach(responsive_menu_option => {
+  responsive_menu_option.addEventListener('click', () => {
+    if (HAMBURGER_ICON.classList.contains('open')) {
+      toggleHamburgerIcon();
+    }
+  });
+});
+window.addEventListener('resize', () => {
+  if (
+    HAMBURGER_ICON.classList.contains('open') &&
+    document.body.clientWidth >= 1024
+  ) {
+    toggleHamburgerIcon();
+  }
+});
+
+// Food and Appetizers Carousel
+const FOOD_AND_APPETIZERS_CONTAINER = document.getElementById(
+  'food-and-appetizers'
+);
+const FOOD_AND_APPETIZER_WIDTH = document.querySelector(
+  '.food-and-appetizer'
+).clientWidth;
+const BACK_ITEM_BUTTON = document.getElementById('backItemButton');
+const NEXT_ITEM_BUTTON = document.getElementById('nextItemButton');
+
+function nextItemOfCarousel(parentDiv, childWidth, parentDivGap) {
+  if (
+    !(parentDiv.offsetWidth + parentDiv.scrollLeft >= parentDiv.scrollWidth)
+  ) {
+    parentDiv.scrollTo(parentDiv.scrollLeft + childWidth + parentDivGap, 0);
+  }
+}
+
+function backItemOfCarousel(parentDiv, childWidth, parentDivGap) {
+  if (parentDiv.scrollLeft) {
+    parentDiv.scrollTo(parentDiv.scrollLeft - (childWidth + parentDivGap), 0);
+  }
+}
+
+NEXT_ITEM_BUTTON.addEventListener('click', () => {
+  nextItemOfCarousel(
+    FOOD_AND_APPETIZERS_CONTAINER,
+    FOOD_AND_APPETIZER_WIDTH,
+    32
+  );
+});
+
+BACK_ITEM_BUTTON.addEventListener('click', () => {
+  backItemOfCarousel(
+    FOOD_AND_APPETIZERS_CONTAINER,
+    FOOD_AND_APPETIZER_WIDTH,
+    32
+  );
+});
